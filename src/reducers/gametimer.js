4efe007781881423e01214bf8385890c4469b84b
ohashi14715369 <ohashi14715369@gmail.com>
2019-05-05 21:09:15 +0900

    redux向けにフォルダ移動、いくつかのredux関連ファイル追加。

---
+++ src/reducers/gametimer.js
@@ -0,0 +1,9 @@
+import * as actionTypes from '../utils/actionTypes';
+const initialAppState = {
+
+};
+const gametimer = (state = initialAppState, action) => {
+    if (action.type === actionTypes.TEST) {
+        return {};
+    }
+};
